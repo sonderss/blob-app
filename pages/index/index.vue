5138f8dab739d224417b261623143008d7f6defb
sonders <778277514@qq.com>
2020-08-25 14:20:11 +0800

    初始化项目

---
+++ pages/index/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="content">
+	   <web-view src="http://sonderss.qicp.vip/blob/#/" :webview-styles="webviewStyles"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				webviewStyles: {
+					progress: {
+						color: '#1e90ff'
+					}
+				}
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
